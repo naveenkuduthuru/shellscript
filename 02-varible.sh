0ce7787ca77fcd6341f8582f2e97bf1d058379ce
daws-76s-github <naveenkuduthuru>
2024-01-12 19:39:05 +0530

    2nd script running

--- 02-varible.sh
+++ 02-varible.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "likhi  :  hi hru where are you now "
-echo "vishnu :  hi iam in hyderbad "
-echo " likhi :  ok when i mett you " 
-echo " vishnu: in uppall"
+
+PERSON1=likhi
+PERSON2=vishnu
+
+
+echo "$PERSON1 :  hi hru  $PERSON2 where are you now "
+echo "$PERSON2:  hi iam in hyderbad $PERSON1"
+echo "$PERSON1:  ok when i mett you $PERSON2" 
+echo "$PERSON2  : in uppall $PERSON1 "
