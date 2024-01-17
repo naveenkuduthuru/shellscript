b5a9f375b9cdf7083318768ad4dead68226f021d
daws-76s-github <naveenkuduthuru>
2024-01-17 15:38:33 +0530

    continue

---
+++ 10-log.sh
@@ -0,0 +1,31 @@
+ID=$(id -u)
+
+echo "script name :$0"
+
+VALIDATE(){
+
+     if [ $1 -ne 0 ]
+  then 
+  echo "ERROR :: $2 ... failed"
+  exit1
+  else 
+  echo "$2 ... SUCCESS"
+  fi
+
+}
+
+  if [ $ID -ne 0 ]
+then 
+  echo "ERROR :: plese run this script with root access"
+  exit1
+  else 
+  echo "you are root user"
+  fi
+
+  yum install mysql -y
+
+VALIDATE $? "installing MySQL"
+  
+  yum install git -y
+
+  VALIDATE $? "installing GIT"
