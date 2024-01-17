553e71f6bd5018f4a22f1a6b84d692bcbb946fd6
daws-76s-github <naveenkuduthuru>
2024-01-17 16:01:08 +0530

    continue

--- 10-log.sh
+++ 10-log.sh
@@ -1,34 +1,34 @@
-ID=$(id -u)
+#!/bin/bash
 
+#!/bin/bash
+
+ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-echo "script name :$0"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+
+
 VALIDATE(){
-
-     if [ $1 -ne 0 ]
-  then 
-  echo "ERROR :: $2 ... failed"
-  exit1
-  else 
-  echo "$2 ... SUCCESS"
-  fi
-
+    if [ $1 -ne 0 ]
+    then
+        echo -e "ERROR:: $2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
-  if [ $ID -ne 0 ]
-then 
-  echo "ERROR :: plese run this script with root access"
-  exit1
-  else 
-  echo "you are root user"
-  fi
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-  yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 
-VALIDATE $? "installing MySQL"  &>> $LOGFILE
-  
-  yum install git -y
+VALIDATE $? "Installing MySQL"
 
-  VALIDATE $? "installing GIT"   &>> $LOGFILE
+yum install git -y &>> $LOGFILE
