7b1192fae8dd67193ed0e0d65900b6f458996c4d
daws-76s-github <naveenkuduthuru>
2024-01-17 16:13:04 +0530

    continue

--- 10-log.sh
+++ 10-log.sh
@@ -7,21 +7,23 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo  "ERROR:: $2 ... $R FAILED $N"
+        echo  -e "ERROR:: $2 ... $R FAILED $N"
         exit 1
     else
-        echo "$2 ... $G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "$R ERROR:: Please run this script with root access $N"
+    echo  -e "$R ERROR:: Please run this script with root access $N"
     exit 1 # you can give other than 0
 else
     echo "You are root user"
