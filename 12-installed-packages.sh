983a1ce4da5fb00037c1c71b3b74499fd0e9f9c2
daws-76s-github <naveenkuduthuru>
2024-01-17 20:42:52 +0530

    continue

---
+++ 12-installed-packages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+Id=$(id -u)
+
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+if [ $ID -ne 0 ]
+then
+    echo  -e    "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+
+
+
+
+
+
+
+
+
